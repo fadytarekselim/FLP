8172a17a4e0ac9d18899b6e472ce494589f98008
Fady Tarek Selim <fadytarekselim@gmail.com>
2018-05-07 21:21:59 +0300

    V1.2 (Fady Tarek)

--- src/IDisplayDataItem.java
+++
@@ -1,6 +0,0 @@
-public interface IDisplayDataItem {    
-    public boolean isDetailAvailable ();
-    public String getDisplayDetail();
-    public int getID();
-    public String getDetailType();    // return simple text String or return the actual object?
-}
